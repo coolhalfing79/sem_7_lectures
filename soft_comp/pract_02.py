92aea6185d46c9e77e7f895e4ffed8ccd654271b
coolhalfing79 <anirudhachari27@gmail.com>
2020-09-27 14:08:32 +0530

    corrected import errors

--- soft_comp/pract_02.py
+++ soft_comp/pract_02.py
@@ -1,6 +1,8 @@
-from ..ANN import Neuron
-import numpy as np
-neuron = Neuron(3, Neuron.arctan, 0)
-neuron._inputs = np.random.random(size=3)
-neuron._weights = np.random.random(size=3)
+import Neuron
+import numpy
+
+
+neuron = Neuron.Neuron(3, Neuron.arctan, 0)
+neuron._inputs = numpy.random.random(size=3)
+neuron._weights = numpy.random.random(size=3)
 print(neuron.calc_out())
