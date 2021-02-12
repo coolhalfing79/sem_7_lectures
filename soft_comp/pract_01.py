4cbc116cef040694f3fd651f46d17e36b80b2d41
anirudha chari <anirudhachari27@gmail.com>
2021-02-13 03:19:00 +0530

    minor changes

--- soft_comp/pract_01.py
+++ soft_comp/pract_01.py
@@ -6,6 +6,7 @@ from hashlib import md5
 import numpy as np
 
 
+np.set_printoptions(precision=2)
 #exe 1
 DATA = 5649
 ATAD = ''
@@ -13,22 +14,23 @@ while DATA > 0:
     ATAD += str(DATA%10)
     DATA = DATA//10
 
-print(int(ATAD))
+print('exe 1\n', int(ATAD))
 
 #exe 2
 NAME = "Anirudha"
 ROLL_NO = 171104008
 GATE_RANK = 1
 PERCENTILE = 99.99
-print(f"name:{NAME} roll no:{ROLL_NO} GATE rank:{GATE_RANK} percentile:{PERCENTILE}")
+print('exe 2\n'+ f"name:{NAME} roll no:{ROLL_NO} GATE rank:{GATE_RANK} percentile:{PERCENTILE}")
 
 #exe3
 players = ['abc', 'de', 'ijk', 'efg', 'lmn', 'op', 'qq', 'rr']
 for player in ['de', 'ijk', 'efg', 'op']:
     players.remove(player)
-print(players)
+print('exe 3\n', players)
 
 #exe4
+print('exe 4')
 N = 6
 for x in range(N):
     print(' ' * (N-x), end='')
@@ -41,15 +43,20 @@ for x in range(N):
 
 
 #exe5
+print('exe 5')
+print('random arrays')
 A1 = np.random.randn(5,4)
 A2 = np.random.randn(3, 4)
+print('A1:\n', A1, '\nA2:\n', A2, '\nthe two arrays joined together')
 A3 = np.concatenate((A1, A2))
+print('A3:\n', A3)
 A4 = np.concatenate((A3, np.random.randn(2, 4)))
 A4 = np.concatenate((A4, np.random.randn(10, 6)), axis=1)
-print(A4.shape)
-# print(A1.T, A2.T, A3.T, A4.T)
+print('A4:\n', A4, '\nshape of new array', A4.shape)
+print('\nA1.T\n', A1.T, '\nA2.T\n', A2.T,'\nA3.T\n', A3.T,'\nA4.T\n', A4.T)
 
 #exe6
+print('exe 6')
 naava = ['Anirudha', 'Bnirudha', 'Cnirudha', 'Dnirudha', 'Enirudha']
 numbers = [1234567890, 2345678901, 3456789012, 4567890123, 5678901234]
 name = {naav:md5(naav.encode()).hexdigest() for naav in naava}
